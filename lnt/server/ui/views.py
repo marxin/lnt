bd455646dc17d9c80ad54fb7e5717d6793b92689
Martin Liska <mliska@suse.cz>
2020-10-15 16:55:38 +0200

    Add new update.

--- lnt/server/ui/views.py
+++ lnt/server/ui/views.py
@@ -933,6 +933,12 @@ def v4_graph():
         'lineWidth': 2,
         'xaxis': {'from': 1595858665000, 'to': 1595858665000},
         })
+    baseline_plots.append({
+        'color': '#ff661a',
+        'lineWidth': 2,
+        'xaxis': {'from': 1602773684511, 'to': 1602773684511},
+        })
+
 
 
     revision_cache = {}
